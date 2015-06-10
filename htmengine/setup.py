29e7a899db9cd50429165ebd3fa99b1b9681d59c
Jared <jcasner@numenta.com>
2015-06-10 11:04:14 -0700

    Revert "Change how NuPIC gets installed"
    This reverts commit eecc9c0bc964fcd256e8cb66fe003292d509665d.

--- htmengine/setup.py
+++ htmengine/setup.py
@@ -1,19 +1,4 @@
-import os
-import sys
-
 from setuptools import setup, find_packages
-
-from infrastructure.utilities.cli import runWithOutput
-
-
-
-# Install NuPIC from the right place; do NuPIC version locking in 
-if "linux" in sys.platform:
-  runWithOutput(("pip install "
-                 "https://s3-us-west-2.amazonaws.com/artifacts.numenta.org/numenta/nupic/releases/nupic-0.2.3-cp27-none-linux_x86_64.whl"),
-                env=os.environ)
-elif "darwin" in sys.platform:
-  runWithOutput("pip install nupic==0.2.3", env=os.environ)
 
 requirements = map(str.strip, open("requirements.txt").readlines())
 
