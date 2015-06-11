a1910b64135e621f90d64d8bebc4e0260f5bc9c8
Jared <jcasner@numenta.com>
2015-06-11 16:43:41 -0700

    try adding dependency_links for linux

--- htmengine/setup.py
+++ htmengine/setup.py
@@ -1,11 +1,16 @@
+import sys
+
 from setuptools import setup, find_packages
 
 requirements = map(str.strip, open("requirements.txt").readlines())
+
+depLinks = ["http://pypi.numenta.com/pypi", ] if "linux" in sys.platform else []
 
 setup(
   name = "htmengine",
   description = "HTM Engine",
   packages = find_packages(),
   include_package_data=True,
-  install_requires = requirements
+  install_requires = requirements,
+  dependency_links = depLinks
 )
