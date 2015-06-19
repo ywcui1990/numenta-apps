cc39223f58f92adfc093ca8f8995d34cb9ef31be
mihail911 <mihail911@gmail.com>
2015-06-19 15:28:10 -0700

    fixed model checkpoint mgr test dependency on grok

--- htmengine/tests/unit/model_checkpoint_mgr/model_checkpoint_mgr_test.py
+++ htmengine/tests/unit/model_checkpoint_mgr/model_checkpoint_mgr_test.py
@@ -28,9 +28,8 @@ from htmengine.model_checkpoint_mgr.model_checkpoint_mgr import \
     ModelCheckpointMgr, ModelNotFound, ModelAlreadyExists
 from nupic.frameworks.opf.modelfactory import ModelFactory
 
-from grok.test_utils.engine.model_checkpoint_test_utils import \
-    ModelCheckpointStoragePatch #TODO: Get rid of this line!
-
+from htmengine.test_utils.model_checkpoint_test_utils import \
+    ModelCheckpointStoragePatch
 
 # Disable warning: Access to a protected member
 # pylint: disable=W0212
@@ -40,12 +39,12 @@ from grok.test_utils.engine.model_checkpoint_test_utils import \
 
 
 
-@ModelCheckpointStoragePatch() #TODO: Get rid of this
+@ModelCheckpointStoragePatch()
 class TestModelCheckpointMgr(unittest.TestCase):
 
 
   def testStorageDirOverrideViaEnvironmentVariable(self):
-    with ModelCheckpointStoragePatch() as storagePatch: #TODO: Get rid of this
+    with ModelCheckpointStoragePatch() as storagePatch:
       checkpointMgr = ModelCheckpointMgr()
 
       tempModelCheckpointDir = storagePatch.tempModelCheckpointDir
