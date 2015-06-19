04920b0525df9c734f6194274e6efd43cd49fa73
Jared <jcasner@numenta.com>
2015-06-19 13:52:32 -0700

    TAUR-1217 try forcing dependency_links to pick up our nupic instead of pypi.org

--- htmengine/setup.py
+++ htmengine/setup.py
@@ -7,7 +7,7 @@ requirements = map(str.strip, open("requirements.txt").readlines())
 
 depLinks = []
 if "linux" in sys.platform and platform.linux_distribution()[0] == "CentOS":
-  depLinks = [ "https://pypi.numenta.com/pypi/nupic#egg=nupic==0.2.3", ]
+  depLinks = [ "https://pypi.numenta.com/pypi/nupic", ]
 
 setup(
   name = "htmengine",
