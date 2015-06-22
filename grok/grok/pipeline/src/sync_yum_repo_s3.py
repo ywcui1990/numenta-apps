d80f09c440350d6c74102b18e3cb8ccc0e65a917
vitaly-krugl <vitaly-krugl@users.noreply.github.com>
2015-06-22 14:59:39 -0700

    Revert "TAUR-1217 Fix stale update-individual-repo"

--- grok/grok/pipeline/src/sync_yum_repo_s3.py
+++ grok/grok/pipeline/src/sync_yum_repo_s3.py
@@ -81,7 +81,8 @@ def syncRepoWithS3(pipelineJson, pipelineParams, repoName):
                 key_filename=serverKey, user=g_config["USER"],
                 connection_attempts=10, warn_only=True):
     g_logger.info("Updating repo to sync S3 repo")
-    result = run("update-individual-repo %s" % repoName)
+    result = run("cd %s && ./update-individual-repo %s" % (
+                 g_config["YUM_REPO_TOOLS_PATH"], repoName))
 
   message = ""
   if result.return_code:
