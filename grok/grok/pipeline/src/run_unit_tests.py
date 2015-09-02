bd641fd6d5c026c0791e6851a5467e108a755ada
Jared <jcasner@numenta.com>
2015-09-02 06:51:55 -0700

    change command to run tests

--- grok/grok/pipeline/src/run_unit_tests.py
+++ grok/grok/pipeline/src/run_unit_tests.py
@@ -118,10 +118,8 @@ def runUnitTests(env, buildWorkspace):
 
   with changeToWorkingDir(os.path.join(buildWorkspace, "numenta-apps", "grok")):
     try:
-      runWithOutput(command=("./run_tests.sh",
-                             "--unit",
-                             "--language py",
-                             "--results xunit jenkins"),
+      runWithOutput(command=("./run_tests.sh --unit --language py --results "
+                             "xunit jenkins"),
                     env=env,
                     logger=g_logger)
     except CommandFailedError:
