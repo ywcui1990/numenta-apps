a004d7734a1acc2635750bf39881e552b43b0a47
Austin Marshall <amarshall@numenta.com>
2015-09-21 14:36:52 -0700

    Fixup result_quality_test to be consistent with cross-platform results

--- htmengine/tests/integration/result_quality_test.py
+++ htmengine/tests/integration/result_quality_test.py
@@ -139,10 +139,10 @@ class ResultQualityTests(test_case_base.TestCaseBase):
     dataIdentifier = "IIO"
     knownDataFile = "iio_us-east-1_i-a2eb1cd9_NetworkIn.csv"
     expectedResults = {"fn": 5,
-                       "fp": 4,
-                       "tn": 891,
+                       "fp": 5,
+                       "tn": 890,
                        "tp": 0,
-                       "quality": -250}
+                       "quality": -300}
 
     results1 = self._runQualityTest(dataIdentifier, knownDataFile,
                                     expectedResults)
