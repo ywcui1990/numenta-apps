bd2db0b7f7b7249e78da075398d38b027f0d5c86
Marion Le Borgne <leborgne.mar@gmail.com>
2015-09-30 13:33:36 -0700

    copyright

--- unicorn/backend/unicorn_backend/package_model_runner/freeze_model_runner.py
+++ unicorn/backend/unicorn_backend/package_model_runner/freeze_model_runner.py
@@ -1,9 +1,34 @@
+# ----------------------------------------------------------------------
+# Numenta Platform for Intelligent Computing (NuPIC)
+# Copyright (C) 2015, Numenta, Inc.  Unless you have purchased from
+# Numenta, Inc. a separate commercial license for this software code, the
+# following terms and conditions apply:
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Affero Public License for more details.
+#
+# You should have received a copy of the GNU Affero Public License
+# along with this program.  If not, see http://www.gnu.org/licenses.
+#
+# http://numenta.org/licenses/
+# ----------------------------------------------------------------------
+
 import cx_Freeze
 import pyproj
 import os
+
 import htmengine
 
+
+
 zipIncludes = []
+
 includeFiles = [(pyproj.pyproj_datadir, os.path.join("pyproj", "data")),
                 ("/".join(htmengine.__file__.split("/")[:-1]) +
                  "/algorithms/modelSelection/anomaly_params_random_encoder"
@@ -12,6 +37,7 @@ includeFiles = [(pyproj.pyproj_datadir, os.path.join("pyproj", "data")),
                  "/anomaly_params_random_encoder/paramOrder.csv")]
 
 executables = [cx_Freeze.Executable("model_runner.py", targetName="mr")]
+
 freezer = cx_Freeze.Freezer(executables,
                             namespacePackages=["nupic", "htmengine",
                                                "prettytable"],
