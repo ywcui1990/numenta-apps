75865efb670294898a8fb64d0458828129c154aa
Austin Marshall <amarshall@numenta.com>
2015-10-08 15:54:41 -0700

    ENG-76 Escape $

--- taurus/pipeline/scripts/update-remote-taurus-servers.sh
+++ taurus/pipeline/scripts/update-remote-taurus-servers.sh
@@ -228,13 +228,13 @@ pushd "${REPOPATH}"
        supervisorctl --serverurl http://localhost:9001 stop all
        let attempts=1
        supervisorctl status | grep -E \"RUNNING|STOPPING\"
-       while [ $? -eq 0  ] && [ $attempts -le 5 ]; do
+       while [ \$? -eq 0  ] && [ \$attempts -le 5 ]; do
          sleep 1
          supervisorctl status | grep -E \"RUNNING|STOPPING\";
          let \"attempts+=1\"
        done
        supervisorctl status | grep -E \"RUNNING|STOPPING\"
-       if [ $? -eq 0 ]; then
+       if [ \$? -eq 0 ]; then
          echo \"Unable to stop all processes...\"
          exit;
        fi
