f8c82f77e04fde21d8ccf8423a739280fd2ec496
Austin Marshall <amarshall@numenta.com>
2015-10-09 10:28:07 -0700

    Revert "Disable console script, but ensure it's installed."
    This reverts commit cb181d16cbbe2508e8af6d49e6c2cf10c8e25283.

--- taurus/pipeline/scripts/update-remote-taurus-servers.sh
+++ taurus/pipeline/scripts/update-remote-taurus-servers.sh
@@ -226,18 +226,7 @@ pushd "${REPOPATH}"
     "cd /opt/numenta/products/taurus &&
      if [ -f taurus-supervisord.pid ]; then
        supervisorctl --serverurl http://localhost:9001 stop all
-       let attempts=1
-       supervisorctl --serverurl http://localhost:9001 status | grep -E \"RUNNING|STOPPING\"
-       while [ \$? -eq 0  ] && [ \$attempts -le 5 ]; do
-         sleep 1
-         supervisorctl --serverurl http://localhost:9001 status | grep -E \"RUNNING|STOPPING\";
-         let \"attempts+=1\"
-       done
-       supervisorctl --serverurl http://localhost:9001 status | grep -E \"RUNNING|STOPPING\"
-       if [ \$? -eq 0 ]; then
-         echo \"Unable to stop all processes...\"
-         exit;
-       fi
+       nta-wait-for-supervisord-processes-stopped --supervisorApiUrl=http://localhost:9001
        supervisorctl --serverurl http://localhost:9001 shutdown
      fi  &&
      if [ -f /var/run/nginx.pid ]; then
