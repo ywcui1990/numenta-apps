16d6d4c93f157f19016acc3bb849aa18fb7a0f4c
Luiz Scheinkman <lscheinkman@numenta.com>
2015-10-18 12:12:27 -0700

    Update files copyright to AGPL

--- taurus-mobile/ios/run_pipeline.sh
+++ taurus-mobile/ios/run_pipeline.sh
@@ -1,3 +1,22 @@
+# Numenta Platform for Intelligent Computing (NuPIC)
+# Copyright (C) 2015, Numenta, Inc.  Unless you have purchased from
+# Numenta, Inc. a separate commercial license for this software code, the
+# following terms and conditions apply:
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Affero Public License for more details.
+#
+# You should have received a copy of the GNU Affero Public License
+# along with this program.  If not, see http://www.gnu.org/licenses.
+#
+# http://numenta.org/licenses/
+
 # Install Pods
 pod install
 
@@ -13,4 +32,3 @@ agvtool new-version -all 1.0.${BUILD_NUMBER}
 xcodebuild clean build archive -workspace taurus.xcworkspace -scheme Taurus -configuration Release -archivePath build/taurus.xcarchive
 rm Taurus.ipa
 xcodebuild -exportArchive -exportPath Taurus -exportFormat ipa -exportProvisioningProfile "${IOS_PROVISIONING_PROFILE}"  -archivePath build/taurus.xcarchive
-
--- taurus-mobile/ios/src/views/AnomalyChartData.swift
+++ taurus-mobile/ios/src/views/AnomalyChartData.swift
@@ -1,10 +1,22 @@
+// Numenta Platform for Intelligent Computing (NuPIC)
+// Copyright (C) 2015, Numenta, Inc.  Unless you have purchased from
+// Numenta, Inc. a separate commercial license for this software code, the
+// following terms and conditions apply:
 //
-//  AnomalyChartData.swift
-//  taurus
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero Public License version 3 as
+// published by the Free Software Foundation.
 //
-//  Created by David Matiskella on 9/6/15.
-//  Copyright © 2015 Numenta. All rights reserved.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Affero Public License for more details.
 //
+// You should have received a copy of the GNU Affero Public License
+// along with this program.  If not, see http://www.gnu.org/licenses.
+//
+// http://numenta.org/licenses/
+
 
 import Foundation
 
