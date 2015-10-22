0b907509a3cfeefb7eae2e0dcaf80708d94be300
Austin Marshall <amarshall@numenta.com>
2015-10-22 12:02:49 -0700

    ENG-27 scaffolding to support `python setup.py test`

--- unicorn/backend/setup.py
+++ unicorn/backend/setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+import os
+import sys
+
 
 
 installRequires = []
 dependencyLinks = []
+
+
 
 with open("requirements.txt", "r") as reqfile:
   for line in reqfile:
@@ -23,11 +29,39 @@ with open("requirements.txt", "r") as reqfile:
 
 
 
+
+class PyTest(TestCommand):
+  testsLocation = os.path.abspath(os.path.join(os.path.basename(__file__),
+                                               "..", "..", "tests", "py"))
+  userOptions = [("pytest-args=", "a", "Arguments to pass to py.test")]
+
+
+  def initialize_options(self):
+    TestCommand.initialize_options(self)
+    self.pytestArgs = [self.testsLocation] # pylint: disable=W0201
+
+
+  def finalize_options(self):
+    TestCommand.finalize_options(self)
+    self.test_args = []
+    self.test_suite = True
+
+
+  def run_tests(self):
+    #import here, cause outside the eggs aren't loaded
+    import pytest
+    errno = pytest.main(self.pytestArgs)
+    sys.exit(errno)
+
+
+
 setup(
   name = "unicorn_backend",
   description = "Unicorn backend",
   packages = find_packages(),
   include_package_data=True,
   install_requires = installRequires,
-  dependency_links = dependencyLinks
+  dependency_links = dependencyLinks,
+  tests_require=["mock==1.0.1", "nta.utils>=0.0.0"],
+  cmdclass = {"test": PyTest},
 )
