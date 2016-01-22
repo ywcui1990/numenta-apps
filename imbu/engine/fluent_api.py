82f3363354647598fed8386f1c132d645ac7e8ce
Alexander Lavin <alavin@numenta.com>
2016-01-21 21:50:26 -0800

    Accomodate for changes to formatResults() in Imbu runner

--- imbu/engine/fluent_api.py
+++ imbu/engine/fluent_api.py
@@ -105,8 +105,10 @@ class FluentWrapper(object):
       g_models[model] = imbu.createModel(model, loadPath, None)
 
     if text:
-      _, idList, sortedDistances = imbu.query(g_models[model], text)
-      return imbu.formatResults(sortedDistances, idList)
+      _, sortedIds, sortedDistances = imbu.query(g_models[model], text)
+      return imbu.formatResults(g_models[model].indexingFactor,
+                                sortedDistances,
+                                sortedIds)
 
     else:
       return []
