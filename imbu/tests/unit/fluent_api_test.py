fabca997d8ead72170a6581069344b351ef7cec1
Austin Marshall <amarshall@numenta.com>
2016-02-12 15:06:18 -0800

    IMBU-120 fluent_api tests

---
+++ imbu/tests/unit/fluent_api_test.py
@@ -0,0 +1,115 @@
+# ----------------------------------------------------------------------
+# Numenta Platform for Intelligent Computing (NuPIC)
+# Copyright (C) 2016, Numenta, Inc.  Unless you have purchased from
+# Numenta, Inc. a separate commercial license for this software code, the
+# following terms and conditions apply:
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see http://www.gnu.org/licenses.
+#
+# http://numenta.org/licenses/
+# ----------------------------------------------------------------------
+
+import contextlib
+import importlib
+import sys
+import unittest
+import json
+from paste.fixture import TestApp
+
+
+@contextlib.contextmanager
+def _tempsyspath(path):
+  sys.path.append(path)
+  yield
+  sys.path.remove(path)
+
+
+class TestFluentAPI(unittest.TestCase):
+
+  def setUp(self):
+    loadPath="/Users/amarshall/nta/numenta-apps/imbu/engine"
+    with _tempsyspath(loadPath):
+      import os
+      os.environ["MODEL_CACHE_DIR"] = "/Users/amarshall/nta/numenta-apps/imbu/cache"
+      os.environ["IMBU_LOAD_PATH_PREFIX"] = loadPath
+      api = importlib.import_module("fluent_api")
+
+    self.app = TestApp(api.app.wsgifunc())
+
+
+  def _assertCORSHeaders(self, response):
+    self.assertIn("access-control-allow-headers", response.header_dict)
+    self.assertEqual(response.header_dict["access-control-allow-headers"],
+                     "accept, access-control-allow-origin, content-type")
+    self.assertIn("access-control-allow-credentials", response.header_dict)
+    self.assertEqual(response.header_dict["access-control-allow-credentials"],
+                     "true")
+    self.assertIn("access-control-allow-headers", response.header_dict)
+    self.assertEqual(response.header_dict["access-control-allow-origin"], "*")
+    self.assertIn("access-control-allow-headers", response.header_dict)
+    self.assertEqual(response.header_dict["access-control-allow-methods"],
+                     "POST")
+
+
+  def testDefaultResponse(self):
+    response = self.app.post("/fluent")
+    self.assertEqual(response.status, 200)
+    self._assertCORSHeaders(response)
+
+    # Assert that response can be parsed as JSON
+    body = json.loads(response.body)
+
+    # Assert structure of response matches expected pattern
+    for textId, result in body.iteritems():
+      self.assertIn("text", result)
+      self.assertIn("scores", result)
+      self.assertEqual(result["scores"], [0])
+
+
+  def _queryModelAndAssertResponse(self, model=None, query="test"):
+    uri = "/fluent"
+    if model:
+      uri = "{}/{}".format(uri, model)
+
+    response = self.app.post(uri, json.dumps(query))
+    self.assertEqual(response.status, 200)
+    self._assertCORSHeaders(response)
+
+    # Assert that response can be parsed as JSON
+    body = json.loads(response.body)
+
+    # Assert structure of response matches expected pattern
+    for textId, result in body.iteritems():
+      self.assertIn("text", result)
+      self.assertIn("scores", result)
+      self.assertIn("windowSize", result)
+
+
+  def testQueryCioWordFingerprint(self):
+    self._queryModelAndAssertResponse("CioWordFingerprint")
+
+
+  def testQueryCioDocumentFingerprint(self):
+    self._queryModelAndAssertResponse("CioDocumentFingerprint")
+
+
+  def testQueryHTMSensorKnn(self):
+    self._queryModelAndAssertResponse("HTM_sensor_knn")
+
+
+  def testQueryHTMSensorSimpleTpKnn(self):
+    self._queryModelAndAssertResponse("HTM_sensor_simple_tp_knn")
+
+
+  def testQueryKeywords(self):
+    self._queryModelAndAssertResponse("Keywords")
