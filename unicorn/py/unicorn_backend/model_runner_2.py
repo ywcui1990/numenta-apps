a28de94fdfd25dd21c3263ee236670633ff8aedc
Luiz Scheinkman <lscheinkman@numenta.com>
2016-02-13 20:23:56 -0800

    UNI-311: Fix pylint issues

--- unicorn/py/unicorn_backend/model_runner_2.py
+++ unicorn/py/unicorn_backend/model_runner_2.py
@@ -35,7 +35,6 @@ released)
 """
 from argparse import ArgumentParser
 import csv
-from datetime import datetime
 import json
 import logging
 import os
@@ -51,7 +50,7 @@ from nupic.data import fieldmeta
 from nupic.data import record_stream
 from nupic.frameworks.opf.modelfactory import ModelFactory
 
-from unicorn_backend import date_time_utils
+import date_time_utils
 
 
 
