437a6b49c8f4c6d3ed77d9b396b2f50b8a3e9152
Brev Patterson <me@brev.name>
2016-02-19 11:26:32 -0800

    UNI-306 #comment Fix File Upload in OOBE branch

--- unicorn/js/browser/actions/FileUpload.js
+++ unicorn/js/browser/actions/FileUpload.js
@@ -23,6 +23,7 @@ import {promiseMetricsFromFiles} from '../lib/Unicorn/FileClient';
 import {TIMESTAMP_FORMATS} from '../lib/Constants';
 import moment from 'moment';
 
+
 /**
  * Upload File Action.
  *  1. Save file metadata {@link FileStore.File}
@@ -42,12 +43,11 @@ export default function (actionContext, payload) {
   let fs = actionContext.getFileClient();
 
   return new Promise((resolve, reject) => {
-
     let file = {
-      uid: Utils.generateFileId(payload.path),
-      name: payload.name,
       filename: payload.path,
-      type: 'uploaded'
+      name: payload.name,
+      type: 'uploaded',
+      uid: Utils.generateFileId(payload.path)
     };
 
     // Save file metadata
@@ -61,36 +61,35 @@ export default function (actionContext, payload) {
         return promiseSaveMetricsToDB(db, metrics);
       }, reject)
       .then((metrics) => {
+        let tsField = metrics.find((metric) => metric.type === 'date').name;
 
-        // Set the file timestampFormat value (2 steps)
-
-        // 1. Get the timestamp field
-        let timestampField = metrics.find((metric) => {
-          return metric.type === 'date';
-        });
-
-        // 2. Get 1 row of data to get the timestamp format
-        let data = [];
-        fs.getData(payload.filename, {limit: 1}, (error, buffer) => {
+        fs.getData(file.filename, {limit: 1}, (error, buffer) => {
+          let datum = null;
           if (error) {
             throw new Error(error);
-          } else if (buffer) {
-            // Guess timestamp format based the first row
-            if (timestampField && data.length === 1) {
-              file.timestampFormat = TIMESTAMP_FORMATS.find((format) => {
-                return moment(data[0][timestampField], format, true).isValid();
-              });
-            }
-            data.push(JSON.parse(buffer));
           }
-        });
+          if (buffer) {
+            datum = JSON.parse(buffer);
 
-        console.log('DEBUG: FileUpload:file', file)
-        // Update stores
-        actionContext.dispatch(ACTIONS.UPLOADED_FILE, file);
-        actionContext.dispatch(ACTIONS.LIST_METRICS, metrics);
-        resolve(file);
+            // Infer timestamp format based the first row
+            if (tsField && datum) {
+              file.timestampFormat = TIMESTAMP_FORMATS.find((format) => {
+                return moment(datum[tsField], format, true).isValid();
+              });
+            } else {
+              throw new Error('Could not find data to process.');
+            }
 
+            if (!(file.timestampFormat)) {
+              throw new Error('Could not find Timestamp Format for this data');
+            }
+
+            // Update stores
+            actionContext.dispatch(ACTIONS.UPLOADED_FILE, file);
+            actionContext.dispatch(ACTIONS.LIST_METRICS, metrics);
+            resolve(file);
+          }
+        }); // fs.getData()
       }, reject);
   });
 }
