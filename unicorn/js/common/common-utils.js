7e9ad420888f7096982d893dc2ce9763a6e5a13c
Luiz Scheinkman <lscheinkman@numenta.com>
2016-03-18 20:42:22 -0700

    UNI-361: Use fixed anomaly scale instead of log scale

--- unicorn/js/common/common-utils.js
+++ unicorn/js/common/common-utils.js
@@ -65,15 +65,20 @@ export function promisify(fn, ...args) {
   });
 }
 
-const LOG_1_MINUS_0_9999999999 = Math.log(1.0 - 0.9999999999);
 /**
- * Return the given value using log scale
- * @param {float} value value
- * @return {float} log scaled value
+ * Return the given value using anomaly scale:
+ *  - [0.99999, 1]  => 0.3
+ *  - [0.9999, 0.99999) => 0.2
+ *  - [0, 0.9999) => 0.1
+ * @param {float} value anomaly value [0 .. 1]
+ * @return {float} scaled value
  */
-export function logScale(value) {
-  if (value > 0.99999) {
-    return 1;
+export function anomalyScale(value) {
+  if (value >= 0.99999) {
+    return 0.3;
   }
-  return Math.log(1.0000000001 - value) / LOG_1_MINUS_0_9999999999;
+  if (value >= 0.9999) {
+    return 0.2;
+  }
+  return 0.1;
 }
